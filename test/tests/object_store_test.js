b1e048f5c4bd593d9d0456c6b2d855242011b805
monocle <hchw29@gmail.com>
2013-08-30 20:50:40 -0700

    Use Karma for testing. Separate tests into multiple files.

---
+++ test/tests/object_store_test.js
@@ -0,0 +1,79 @@
+
+asyncTest("adding, getting, putting, and removing a record", function() {
+  expect(6);
+
+  EIDB.open("foo", 1, function(db) {
+    start();
+    var store = db.createObjectStore("people", { keyPath: "id" });
+  }).then(function(db) {
+    var tx = db.transaction(["people"], "readwrite"),
+        store = tx.objectStore("people");
+
+    var req = store.add({id: 1, name: "Erik"});
+    stop();
+    req.then(function(event) {
+      start();
+      ok(event, "Event was passed in when resolved");
+
+      stop();
+      store.get(1).then(function(obj) {
+        start();
+
+        equal(obj.id, 1);
+        equal(obj.name, "Erik");
+
+        stop();
+        obj.name = "Kris";
+        store.put(obj).then(function(obj) {
+          start();
+
+          equal(obj.id, 1);
+          equal(obj.name, "Kris");
+        });
+      });
+
+      stop();
+      store.delete(1).then(function(event) {
+        start();
+        ok(event, "Event was passed in when resolved");
+
+        db.close();
+      });
+    });
+  });
+});
+
+asyncTest('ObjectStore API - indexes', function() {
+  expect(7);
+
+  EIDB.open('foo', 1, function(db) {
+    var _index, index, indexNames,
+        store = db.createObjectStore("people", { keyPath: "myId" });
+
+    _index = store.createIndex('by_name', 'name', {unique: true});
+
+    index = store.index('by_name');
+    indexNames = store.indexNames;
+
+    ok(_index instanceof EIDB.Index, "#createIndex returns an EIDB.Index");
+    ok(index instanceof EIDB.Index, "#index returns an EIDB.Index");
+    ok(index.unique, '#createIndex passes along params');
+    ok(indexNames instanceof DOMStringList, "#indexNames returns a DOMStringList");
+    ok(indexNames.contains('by_name'), '#indexNames contains the names of the indexes');
+
+    store.deleteIndex('by_name');
+    ok(!store.indexNames.contains('by_name'), '#deleteIndex removes the index');
+
+    store.createIndex('by_name', 'name', {unique: true});
+  }).then(function(db) {
+    db.close();
+
+    EIDB.open('foo', 2).then(function(db) {
+      var store = db.transaction('people').objectStore('people');
+      ok(store.indexNames.contains('by_name'), "new store object finds an existing index");
+
+      db.close();
+      start();
+    });
+  });
+});
